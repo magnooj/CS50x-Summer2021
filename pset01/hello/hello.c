37a2c83d7134379e033fa1d4d3c803def18415b8
magnooj <76149617+magnooj@users.noreply.github.com>
2021-12-07 12:51:09 +0330

    Delete pset01 directory

--- pset01/hello/hello.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <cs50.h>
-
-int main(void)
-{
-    string answer = get_string("What is your name?\n");
-    //This string is getting your name.
-    printf("Hello, %s\n", answer);
-}
